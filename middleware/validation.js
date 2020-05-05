5099312c10197406f82536532ffce0fc7bde55b6
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-05 04:21:29 +0100

    add signup logic

---
+++ middleware/validation.js
@@ -0,0 +1,40 @@
+const {
+    body,
+    validationResult
+} = require('express-validator');
+
+exports.signUpValidationRules = () => {
+    return [
+        // username must be an email
+        body("first_name").notEmpty().isAlpha().trim().escape().withMessage("Name must not be empty"),
+        body("last_name").notEmpty().isAlpha().trim().escape().withMessage("Name must not be empty"),
+        body("role").notEmpty().isIn(["Student", "Tutor", "Admin"]).withMessage("Role should be 'Student' or 'Tutor' "),
+        body("email").notEmpty().isEmail().normalizeEmail().withMessage("Email is required"),
+        body("password").notEmpty().isLength({
+            min: 5
+        }).withMessage("Password must have at least 5 characters"),
+    ]
+}
+
+exports.validate = (req, res, next) => {
+    try {
+        const errors = validationResult(req)
+        if (errors.isEmpty()) {
+            return next()
+        }
+        const extractedErrors = []
+        errors.array().map(err => extractedErrors.push({
+            [err.param]: err.msg
+        }))
+
+        return res.status(422).json({
+            errors: extractedErrors,
+        })
+
+    } catch {
+        res.status(401).json({
+            error: "Unauthorized",
+            status: "error"
+        })
+    }
+}
