99585138e7785cf964bb0b85ae201de7c63b954e
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-06 22:50:50 +0100

    add deactivate tutor logic

--- controllers/tutor.js
+++ controllers/tutor.js
@@ -22,11 +22,39 @@ exports.getAllTutors = (req, res, next) => {
 
 exports.getTutorById = (req, res, next) => {
     const _id = req.params.id;
-    User.find({_id}).then((tutor) => {
+    const filter = {
+        role: "Tutor",
+        _id
+    };
+    User.find(filter).then((tutor) => {
         if (tutor) {
             return res.status(200).send({
                 status: true,
                 data: tutor
+            });
+        }
+    }).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    )
+}
+
+exports.deactivateTutorById = (req, res, next) => {
+    const _id = req.params.id;
+    const filter = {
+        role: "Tutor",
+        _id
+    };
+    User.findOneAndDelete(filter).then((tutor) => {
+        console.log(tutor);
+        if (tutor) {
+            return res.status(200).send({
+                status: true,
+                message: "Tutor was deactivated successfully",
+                name: tutor.name
             });
         }
     }).catch(
