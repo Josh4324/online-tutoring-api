dadcddb7177fedce228e19d2350d0776469afd89
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-06 22:20:17 +0100

    add search for tutor by first name

--- routes/search.js
+++ routes/search.js
@@ -7,6 +7,7 @@ const validation = require("../middleware/validation");
 
 const userController = require('../controllers/search');
 
-router.get('/subject', userController.searchBySubjectName);
+router.get('/subject', auth.authentication,userController.searchBySubjectName);
+router.get('/tutors', auth.authentication,userController.searchByTutorFirstName);
 
 module.exports = router;
