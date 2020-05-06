aedfa208098a5efc6b3d0e6c0d7fe9f6d0f3bbd6
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-06 23:58:54 +0100

    add delete lesson by id

--- routes/lesson.js
+++ routes/lesson.js
@@ -9,5 +9,5 @@ router.get('/', auth.adminAuthorization, userController.getAllLessons);
 router.get('/:id', auth.adminAuthorization, userController.getLessonById);
 router.post('/', auth.adminAndSudentAuthorization, validation.bookPostValidationRules(), validation.validate, userController.bookLesson);
 router.put('/:id', auth.adminAuthorization, validation.bookPostValidationRules(),validation.validate, userController.updateLessonById);
-
+router.delete('/:id',auth.adminAuthorization, userController.deleteLessonById);
 module.exports = router;
