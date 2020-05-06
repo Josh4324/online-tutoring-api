777cdeb3e7cf1d5f0ad371aa94141beb01a8426a
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-06 15:05:23 +0100

    add delete category logic

--- controllers/category.js
+++ controllers/category.js
@@ -61,11 +61,27 @@ exports.updateCategory = (req, res, next) => {
 exports.getAllCategories = (req, res, next) => {
     const filter = {};
     Category.find(filter).then((categories) => {
-        if(categories){
+        if (categories) {
             return res.status(200).send({
                 status: true,
-                data:categories
+                data: categories
             });
         }
     })
+}
+
+exports.deleteCategory = (req, res, next) => {
+    const category_name = req.params.category_name;
+    const filter = {
+        name: category_name
+    };
+    Category.findOneAndDelete(filter).then((category) => {
+        if (category) {
+            return res.status(200).send({
+                status: true,
+                message: "Category was deleted successfully",
+                name: category.name
+            });
+        }
+    }).catch((err) => console.log(err));
 }
