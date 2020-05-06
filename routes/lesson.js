7c6a2aab692df0e7d3b62cde03ea68be00655d05
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-06 23:38:58 +0100

    add get lesson by id logic

--- routes/lesson.js
+++ routes/lesson.js
@@ -6,6 +6,7 @@ const validation = require("../middleware/validation");
 const userController = require('../controllers/lesson');
 
 router.get('/', auth.adminAuthorization, userController.getAllLessons);
+router.get('/:id', auth.adminAuthorization, userController.getLessonById);
 router.post('/', auth.adminAndSudentAuthorization, validation.bookPostValidationRules(), validation.validate, userController.bookLesson);
 
 
