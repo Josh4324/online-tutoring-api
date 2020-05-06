4656d26a1c97206731e771469484883d34e5d700
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-06 22:06:58 +0100

    add get tutors logic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
 const searchRoutes = require('./routes/search');
+const tutorRoutes = require('./routes/tutor');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -18,6 +19,7 @@ app.use(express.urlencoded({
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/categories', categoryRoutes);
 app.use('/api/v1/search',searchRoutes)
+app.use('/api/v1/tutors',tutorRoutes)
 
 
 app.use((req, res) => {
